5f6c7dde3838ebb3bc7c5f357f9c86bbb791fe1e
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-16 03:16:40 +0200

    update

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,15 @@
 export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
+
   socials: {
-    twitter: 'Atinux',
+    twitter: 'SoftEng_Islam',
     github: 'Atinux/content-wind'
+  },
+
+  nuxtIcon: {
+    aliases: {
+      ' i-ph-twitter-logo-fill ': 'i-ph-twitter-logo'
+    }
   }
 })
